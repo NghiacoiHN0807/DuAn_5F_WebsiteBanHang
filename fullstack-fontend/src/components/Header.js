12caa0168de285fe05af19a099758739cccd5c2f
NghiacoiHN0807 <ytbcointn@gmail.com>
2023-06-30 20:40:24 +0700

    fix-entity-backend

--- fullstack-fontend/src/components/Header.js
+++ fullstack-fontend/src/components/Header.js
@@ -7,28 +7,43 @@ import { NavLink } from "react-router-dom";
 const Header = (props) => {
   // const location = useLocation();
   return (
-    <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+    <>
       <Container>
-        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
-        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-        <Navbar.Collapse id="responsive-navbar-nav">
-          <Nav className="me-auto">
-            <NavLink to="/" className={"nav-link"}>
-              Home
-            </NavLink>
-            <NavLink to="/table-xuatXu" className={"nav-link"}>
-              Table Xuat Xu
-            </NavLink>
-          </Nav>
-          <Nav>
-            <NavDropdown title="Setting">
-              <NavDropdown.Item href="#">Login</NavDropdown.Item>
-              <NavDropdown.Item href="#">Logout</NavDropdown.Item>
-            </NavDropdown>
-          </Nav>
-        </Navbar.Collapse>
+        <Nav className="justify-content-end" activeKey="/home">
+          <Nav.Item>
+            <Nav.Link href="/home">Help</Nav.Link>
+          </Nav.Item>
+          <Nav.Item>
+            <Nav.Link eventKey="link-1">Join Us</Nav.Link>
+          </Nav.Item>
+          <Nav.Item>
+            <Nav.Link eventKey="link-2">Login</Nav.Link>
+          </Nav.Item>
+        </Nav>
       </Container>
-    </Navbar>
+      <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+        <Container>
+          <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+          <Navbar.Collapse id="responsive-navbar-nav">
+            <Nav className="me-auto">
+              <NavLink to="/" className={"nav-link"}>
+                Home
+              </NavLink>
+              <NavLink to="/table-xuatXu" className={"nav-link"}>
+                Table Xuat Xu
+              </NavLink>
+            </Nav>
+            <Nav>
+              <NavDropdown title="Setting">
+                <NavDropdown.Item href="#">Login</NavDropdown.Item>
+                <NavDropdown.Item href="#">Logout</NavDropdown.Item>
+              </NavDropdown>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </>
   );
 };
 export default Header;
