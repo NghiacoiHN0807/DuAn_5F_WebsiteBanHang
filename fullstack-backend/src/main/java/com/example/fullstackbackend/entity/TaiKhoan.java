d7c559d5d552c5856a18bf04e2c02e658c569d42
NghiacoiHN0807 <ytbcointn@gmail.com>
2023-07-27 00:37:58 +0700

    add-entity

---
+++ fullstack-backend/src/main/java/com/example/fullstackbackend/entity/TaiKhoan.java
@@ -0,0 +1,59 @@
+package com.example.fullstackbackend.entity;
+
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Table(name = "tai_khoan")
+public class TaiKhoan {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_tai_khoan")
+    private Integer idTaiKhoan;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "id_dia_chi", referencedColumnName = "id_dia_chi")
+    private DiaChi idDiaChi;
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "id_chuc_vu", referencedColumnName = "id_cv")
+    private ChucVu idChucVu;
+
+    @Column(name = "ma_tai_khoan")
+    private String maTaiKhoan;
+
+    @Column(name = "ho")
+    private String ho;
+
+    @Column(name = "ten")
+    private String ten;
+
+    @Column(name = "sdt")
+    private String sdt;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "mat_khau")
+    private String matKhau;
+
+    @Column(name = "trang_thai")
+    private Integer trangThai;
+
+}
+
