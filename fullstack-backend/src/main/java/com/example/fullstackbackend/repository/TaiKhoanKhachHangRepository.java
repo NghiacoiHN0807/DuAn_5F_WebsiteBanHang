0c0d1f0f77ee79f611662c8386484b022ae52a2a
MSI\latha <lathanhjk@gmail.com>
2023-08-06 17:39:52 +0700

    no message

--- fullstack-backend/src/main/java/com/example/fullstackbackend/repository/TaiKhoanKhachHangRepository.java
+++ fullstack-backend/src/main/java/com/example/fullstackbackend/repository/TaiKhoanKhachHangRepository.java
@@ -1,7 +1,14 @@
 package com.example.fullstackbackend.repository;
 
 import com.example.fullstackbackend.entity.TaiKhoanKhachHang;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface TaiKhoanKhachHangRepository extends JpaRepository<TaiKhoanKhachHang, Integer> {
+
+    @Query("select t from TaiKhoanKhachHang t order by t.idTaiKhoan DESC ")
+    @Override
+    Page<TaiKhoanKhachHang> findAll(Pageable pageable);
 }
