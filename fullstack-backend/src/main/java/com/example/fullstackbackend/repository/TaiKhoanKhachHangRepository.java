d718b95404daed406c3df5b1fabfca8f9c8e4d22
MSI\latha <lathanhjk@gmail.com>
2023-08-08 09:58:56 +0700

    Sửa lại Repo tài khoản khách hàng

--- fullstack-backend/src/main/java/com/example/fullstackbackend/repository/TaiKhoanKhachHangRepository.java
+++ fullstack-backend/src/main/java/com/example/fullstackbackend/repository/TaiKhoanKhachHangRepository.java
@@ -8,7 +8,7 @@ import org.springframework.data.jpa.repository.Query;
 
 public interface TaiKhoanKhachHangRepository extends JpaRepository<TaiKhoan, Integer> {
 
-    @Query("select t from TaiKhoan t order by t.idTaiKhoan DESC ")
+    @Query("select t from TaiKhoan t where t.idChucVu is null  order by t.idTaiKhoan DESC ")
     @Override
     Page<TaiKhoan> findAll(Pageable pageable);
 }
