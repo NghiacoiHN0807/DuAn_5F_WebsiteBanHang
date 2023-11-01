1cf762fece5ff62f5a4cd013073746bd02e4bf2b
NghiacoiHN0807 <ytbcointn@gmail.com>
2023-11-01 18:19:05 +0700

    commit-security

--- fullstack-backend/src/main/java/com/example/fullstackbackend/config/MySecurityConfiguration.java
+++ fullstack-backend/src/main/java/com/example/fullstackbackend/config/MySecurityConfiguration.java
@@ -2,11 +2,13 @@ package com.example.fullstackbackend.config;
 
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
-import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
@@ -16,7 +18,6 @@ import org.springframework.security.web.SecurityFilterChain;
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
-@EnableMethodSecurity(prePostEnabled = true)
 public class MySecurityConfiguration {
 
     private final UserService userService;
@@ -31,19 +32,18 @@ public class MySecurityConfiguration {
         return PasswordEncoderFactories.createDelegatingPasswordEncoder();
     }
 
-    //    @Bean
-//    public void configure(AuthenticationManagerBuilder auth) throws Exception {
-//        System.out.println("auth: " + auth);
-//        auth.userDetailsService(userService) // Provide userservice for spring security
-//                .passwordEncoder(passwordEncoder()); // Provide password encoder
-//    }
+    @Primary
     @Bean
-    public AuthenticationManager authenticationManager(AuthenticationConfiguration auth) throws Exception {
-        return auth.getAuthenticationManager();
+    public AuthenticationManagerBuilder configureAuth(AuthenticationManagerBuilder auth) throws Exception {
+        System.out.println("auth: " + auth);
+        auth.userDetailsService(userService) // Provide userservice for spring security
+                .passwordEncoder(passwordEncoder()); // Provide password encoder
+        return auth;
     }
 
+    @Qualifier
     @Bean
-    public SecurityFilterChain configure(HttpSecurity http) throws Exception {
+    public SecurityFilterChain configureHttp(HttpSecurity http) throws Exception {
         System.out.println("http: " + http);
         return http.authorizeHttpRequests(
                         req ->
