45d9f3a85e614238f8fd8dfd7610cde3ed1ca867
longdeptrai09 <tranhoanglongdeptrai09@gmail.com>
2023-11-13 16:09:14 +0700

    16:10 13/11/23

--- fullstack-fontend/src/components/Header.js
+++ fullstack-fontend/src/components/Header.js
@@ -1,27 +1,43 @@
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
-import NavDropdown from "react-bootstrap/NavDropdown";
+// import NavDropdown from "react-bootstrap/NavDropdown";
 import { NavLink } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCartShopping, faHeart } from "@fortawesome/free-solid-svg-icons";
 // useLocation,
 const Header = (props) => {
   // const location = useLocation();
   return (
     <>
-      <Container>
-        <Nav className="justify-content-end" activeKey="/home">
-          <Nav.Item>
-            <Nav.Link href="/home">Help</Nav.Link>
-          </Nav.Item>
-          <Nav.Item>
-            <Nav.Link eventKey="link-1">Join Us</Nav.Link>
-          </Nav.Item>
-          <Nav.Item>
-            <Nav.Link eventKey="link-2">Login</Nav.Link>
-          </Nav.Item>
-        </Nav>
-      </Container>
-      <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+      <div className="gray-background">
+        <Container>
+          <Nav className="justify-content-end" activeKey="/home">
+            <Nav.Item>
+              <Nav.Link href="/home" className="nav-links">
+                Help
+              </Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+              <span className="nav-separator">|</span>
+            </Nav.Item>
+            <Nav.Item>
+              <Nav.Link eventKey="link-1" className="nav-links">
+                Join Us
+              </Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+              <span className="nav-separator">|</span>
+            </Nav.Item>
+            <Nav.Item>
+              <Nav.Link eventKey="link-2" className="nav-links">
+                Login
+              </Nav.Link>
+            </Nav.Item>
+          </Nav>
+        </Container>
+      </div>
+      <Navbar collapseOnSelect expand="lg" bg="while" variant="while">
         <Container>
           <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
           <Navbar.Toggle aria-controls="responsive-navbar-nav" />
@@ -35,10 +51,12 @@ const Header = (props) => {
               </NavLink>
             </Nav>
             <Nav>
-              <NavDropdown title="Setting">
-                <NavDropdown.Item href="#">Login</NavDropdown.Item>
-                <NavDropdown.Item href="#">Logout</NavDropdown.Item>
-              </NavDropdown>
+              <NavLink to="/table-xuatXu" className={"nav-link"}>
+                <FontAwesomeIcon icon={faHeart} size="lg" />
+              </NavLink>
+              <NavLink to="/table-xuatXu" className={"nav-link"}>
+                <FontAwesomeIcon icon={faCartShopping} size="lg" />
+              </NavLink>
             </Nav>
           </Navbar.Collapse>
         </Container>
