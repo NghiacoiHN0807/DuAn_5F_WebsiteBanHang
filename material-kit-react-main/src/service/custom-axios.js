251565e1a8deae9392a883492148e5c0f60de86d
NghiacoiHN0807 <ytbcointn@gmail.com>
2023-11-19 13:16:29 +0700

    hi

--- material-kit-react-main/src/service/custom-axios.js
+++ material-kit-react-main/src/service/custom-axios.js
@@ -1,11 +1,12 @@
 import axios from 'axios';
+import { accessToken } from '../sections/auth/login/LoginForm'; // Đường dẫn tới file token.js
 
 const token = localStorage.getItem('accessToken');
 
 const instance = axios.create({
   baseURL: 'http://localhost:8080',
   headers: {
-    Authorization: token && `Bearer ${token}`,
+    Authorization: token ? `Bearer ${token}` : `Bearer ${accessToken}`,
   },
 });
 
