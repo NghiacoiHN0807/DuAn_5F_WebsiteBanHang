f813d85a60b3cabcda9145718ec83ff687ca1901
longdeptrai09 <tranhoanglongdeptrai09@gmail.com>
2023-12-05 16:40:50 +0700

    16:40 05/12/23

--- fullstack-backend/src/main/java/com/example/fullstackbackend/config/MySecurityConfiguration.java
+++ fullstack-backend/src/main/java/com/example/fullstackbackend/config/MySecurityConfiguration.java
@@ -58,20 +58,17 @@ public class MySecurityConfiguration {
     protected SecurityFilterChain configureHttp(HttpSecurity http) throws Exception {
         System.out.println("http: " + http);
         return http.authorizeHttpRequests(
-                        req ->
-                                req.requestMatchers("/", "/anh/**", "/gio-hang-chi-tiet/**", "/add", "/api/**", "/san-pham/**", "chi-tiet-san-pham/**", "/hoa-don/**", "/hoa-don-chi-tiet/**", "/tai-khoan-khach-hang/**", "/chat-lieu/**", "/loai-sp/**", "/xuat-xu/**", "/loai-co-ao/**", "/ong-tay-ao/**", "/mau-sac/**", "/size/**").permitAll()
-                                        .requestMatchers("/tai-khoan/**").hasRole("ADMIN")
-                                        .requestMatchers("/giam-gia/**", "/giam-gia-chi-tiet/**").hasAnyRole("ADMIN", "STAFF")
-                                        .requestMatchers("/tai-khoan-khach-hang/**", "/hoa-don/**", "/hoa-don-chi-tiet/**", "/gio-hang-chi-tiet/**", "/gio-hang/**", "/payment-online/**", "/hinh-thuc-thanh-toan/**", "/lich-su-hoa-don/**").hasAnyRole("ADMIN", "STAFF", "CUSTOMER")
-                                        .anyRequest().authenticated())
-//                .oauth2Login(oath2 -> {
-//                    oath2.successHandler(oAuth2LoginSuccessHandler);
-//                })
+
+                req ->
+                        req.requestMatchers("/", "/anh/**", "/gio-hang-chi-tiet/**", "/add", "/api/**", "/san-pham/**", "chi-tiet-san-pham/**", "/hoa-don/**", "/hoa-don-chi-tiet/**", "/tai-khoan-khach-hang/**", "/chat-lieu/**", "/loai-sp/**", "/xuat-xu/**", "/loai-co-ao/**", "/ong-tay-ao/**", "/mau-sac/**", "/size/**").permitAll()
+                                .requestMatchers("/tai-khoan/**").hasRole("ADMIN")
+                                .requestMatchers("/giam-gia/**", "/giam-gia-chi-tiet/**").hasAnyRole("ADMIN", "STAFF")
+                                .requestMatchers("/tai-khoan-khach-hang/**", "/hoa-don/**", "/hoa-don-chi-tiet/**", "/gio-hang-chi-tiet/**", "/gio-hang/**", "/payment-online/**", "/hinh-thuc-thanh-toan/**", "/lich-su-hoa-don/**", "/dia-chi/**").hasAnyRole("ADMIN", "STAFF", "CUSTOMER")
+                                .anyRequest().authenticated())
                 .addFilterBefore(jwtAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class)
                 .csrf(csrf -> csrf.disable())
                 .cors(a -> a.configure(http))
                 .build();
-
     }
 
 }
