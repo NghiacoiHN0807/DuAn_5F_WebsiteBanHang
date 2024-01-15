9e2b9fee0970a709b00b32390ad7e97316b712c8
VinhLT77 <lethevinh147@gmail.com>
2024-01-15 23:23:21 +0700

    bip

--- fullstack-backend/src/main/java/com/example/fullstackbackend/controller/HoaDonController.java
+++ fullstack-backend/src/main/java/com/example/fullstackbackend/controller/HoaDonController.java
@@ -188,15 +188,12 @@ public class HoaDonController {
         if (newHD.getTrangThai() == 5 && hoaDonCheck.getThanhTien().compareTo(money) > 0) {
             nameError = "Số Tiền Thanh Toán Chưa Đủ";
             hasError = true;
-<<<<<<< HEAD
-=======
         } else if (hoaDonCheck.getTrangThai() == 6) {
             nameError = "Hóa Đơn Đã Bị Hủy";
             hasError = true;
         } else if (hoaDonCheck.getTrangThai() == 15) {
             nameError = "Hóa Đơn Đã Hủy Do Giao Không Thành Công";
             hasError = true;
->>>>>>> origin/main
         }
 
         //Done hasError
